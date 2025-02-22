0a8858fa19d398b7a19423a526aa128527f2a576
Albertojg05 <alberto.jimenez252595@potros.itson.edu.mx>
2025-02-21 19:32:04 -0700

    Solo comentarios porque se me olvido cambiarlos

--- src/Interfaces/InterfazPerfilHistorial.java
+++ src/Interfaces/InterfazPerfilHistorial.java
@@ -20,53 +20,58 @@ public class InterfazPerfilHistorial {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Frame
+        // JFrame
         JFrame frame = new JFrame("Historial");
         frame.setSize(630, 477);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);
         
+        // Todo lo que se utiliza dentro del JFrame
         JLabel TicketWizard = new JLabel("TicketWizard");
         JButton botonRegresar = new JButton("Regresar");
         JButton botonHistorialTransacciones = new JButton("Transacciones");
         JButton botonHistorialBoletos = new JButton("Boletos");
         JButton botonHistorialReventas = new JButton("Reventas");
         
+        // Igual que se vea bien el programa
         TicketWizard.setFont(new Font("Racing Sans One", Font.BOLD, 58));
         TicketWizard.setForeground(Color.BLUE);
         TicketWizard.setBounds(20, 10, 600, 70);
         
+        // Solo es el formato para que se vean bonitos
         botonRegresar.setBackground(new Color(0xEC, 0x22, 0x1F));
         botonRegresar.setForeground(Color.WHITE);
         botonRegresar.setFont(new Font("Arial", Font.BOLD, 12));
         botonRegresar.setSize(90, 36);
         botonRegresar.setLocation(frame.getWidth() - botonRegresar.getWidth() - 35, 30);
         
-        // Porque dio webis acomodar bien 
+        // Lo que hace es que los acomoda por debajo del principal que en este caso es el 
+        // boton historial transacciones
         int posX = frame.getWidth() - botonHistorialTransacciones.getWidth() - 375;
         
-        // Boton de la reventa a pesar de que revender es ilegal :0
+        // Boton del historial de transacciones hechas
         botonHistorialTransacciones.setFont(new Font("Arial", Font.BOLD, 14));
         botonHistorialTransacciones.setBackground(new Color(0x65, 0x55, 0x8F)); 
         botonHistorialTransacciones.setForeground(Color.BLACK);
         botonHistorialTransacciones.setSize(160, 40);
         botonHistorialTransacciones.setLocation(posX, 150);
         
-        // boton del historial locochon
+        // boton del historial de boletos comprados
         botonHistorialBoletos.setFont(new Font("Arial", Font.BOLD, 14));
         botonHistorialBoletos.setBackground(new Color(0x65, 0x55, 0x8F)); 
         botonHistorialBoletos.setForeground(Color.BLACK);
         botonHistorialBoletos.setSize(160, 40);
         botonHistorialBoletos.setLocation(posX, 215);
         
-        // Boton de saldo para el pobreton ese :(
+        // Boton de historial de reventas 
         botonHistorialReventas.setFont(new Font("Arial", Font.BOLD, 14));
         botonHistorialReventas.setBackground(new Color(0x65, 0x55, 0x8F)); 
         botonHistorialReventas.setForeground(Color.BLACK);
         botonHistorialReventas.setSize(160, 40);
         botonHistorialReventas.setLocation(posX, 285);
         
+        // Aún faltan las acciones de cada botón para conectarlos al principal
         botonRegresar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {                
@@ -98,6 +103,8 @@ public class InterfazPerfilHistorial {
         frame.add(botonHistorialTransacciones);
         frame.add(botonHistorialBoletos);
         frame.add(botonHistorialReventas); 
+        
+        //frame.getContentPane().setBackground(new Color(173, 216, 230)); "Solo era para probar"
         
         frame.setVisible(true);
     }
